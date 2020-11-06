9923d299c017a07591b5ad1b05e60f7e7c31f9a2
evelinenobrega <eveline.lorrane@gmail.com>
2020-11-06 15:20:24 -0300

    Add function %
    Manutenção no código para a execução do cálculo de porcentagem de forma correta

--- script.js
+++ script.js
@@ -10,7 +10,16 @@ function reseta(){
     document.calc.visor.value = '';
 }
 
-function calcula() {
-    resultado = eval(valor);
+  function executa() {
+    if(!valor.includes('%')) {
+        resultado = calcula(valor);
+    } else {
+        valor = valor.replace("%", "") + "/100";
+        resultado = calcula(valor);
+    }
     document.calc.visor.value = resultado.toLocaleString("pt-BR");
-  }
+}
+
+function calcula(valor) {
+    return eval(valor);
+}
